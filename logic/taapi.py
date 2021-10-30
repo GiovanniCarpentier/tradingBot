34e78d95b648a4e72743bc405fac490ccb7c5168
Giovanni Carpentier <giovanni.carpentier@student.howest.be>
2021-10-30 12:38:00 +0200

    try catch taapi

--- logic/taapi.py
+++ logic/taapi.py
@@ -35,22 +35,28 @@ parameters = {
 
 
 def getData():
-    # Send request to TAAPI and save it to response
-    response = requests.post(url=endpoint, json=parameters)
+    try:
+        # Send request to TAAPI and save it to response
+        response = requests.post(url=endpoint, json=parameters)
 
-    # Get the response JSON
-    result = response.json()
+        # Get the response JSON
+        result = response.json()
 
-    # Create empty array to store price data
-    prices = []
+        # Create empty array to store price data
+        prices = []
 
-    # Get the data from the JSON
-    data = result.get("data")
+        # Get the data from the JSON
+        data = result.get("data")
 
-    # For every item in data get the result and the value in the result and add it to the price array
-    prices.append(data[0]["result"]["value"])  # WMA // 0
-    prices.append(data[1]["result"]["value"])  # VWMA // 1
-    prices.append(data[2]["result"]["open"])  # PRICE // 2
-    prices.append(data[3]["result"]["value"])  # RSI // 3
+        # For every item in data get the result and the value in the result and add it to the price array
+        prices.append(data[0]["result"]["value"])  # WMA // 0
+        prices.append(data[1]["result"]["value"])  # VWMA // 1
+        prices.append(data[2]["result"]["open"])  # PRICE // 2
+        prices.append(data[3]["result"]["value"])  # RSI // 3
 
-    return prices
+        send(messages=[data])
+
+        return prices
+
+    except Exception as e:
+        send(messages=["ERROR while fetching TAAPI data"])
